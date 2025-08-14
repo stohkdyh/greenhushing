3cb1833df46c424f5b4141778cc59251c165cd37
elsazhr <elsazahra28@gmail.com>
2025-08-14 21:57:36 +0700

    benerke logo di footer per product

--- resources/views/onephone/overview/footer.blade.php
+++ resources/views/onephone/overview/footer.blade.php
@@ -1,6 +1,6 @@
 <x-footer 
     brand="Onephone" 
-    logo="images/logo_claim_green.png"
+    logo="images/EU_Ecolabel_Logo.svg"
     bg="bg-[#163F22]" 
     newsletterTitle="Onephone Newsletter"
     newsletterDesc="Stay up to date with the latest news and stories from Onephone."
