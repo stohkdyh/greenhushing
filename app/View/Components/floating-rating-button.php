14c8e783afed2b81858454da932cdd42c74d154e
stohkdyh <adityaekapret@gmail.com>
2026-06-20 00:31:00 +0700

    feat: create FloatingRatingButton view component

--- app/View/Components/floating-rating-button.php
+++ app/View/Components/floating-rating-button.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class floating-rating-button extends Component
+class FloatingRatingButton extends Component
 {
     /**
      * Create a new component instance.
